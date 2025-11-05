d511e73cb9907555358cbef17a2ead304257b829
thanhtoan <duongthanhtoan105@gmail.com>
2025-11-05 14:49:08 +0000

    Remove unwanted files and folders

--- docker/postgres-init/01_schema.sql
+++
@@ -1,8 +0,0 @@
--- Create schema if not exists
-CREATE SCHEMA IF NOT EXISTS accounting;
-
--- Set default search_path at the database level (applies to new sessions)
-ALTER DATABASE accounting_dev SET search_path = accounting, public;
-
--- Ensure role-level default also prefers accounting
-ALTER ROLE accounting SET search_path = accounting, public;
